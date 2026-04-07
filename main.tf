23b69218bb14f771eea28fb5ac1520f8f5652f82
edanik90 <e.danik90@gmail.com>
2026-04-07 13:45:53 -0500

    changed the name of the subnet_2

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "azurerm_subnet" "subnet_1" {
 }
 
 resource "azurerm_subnet" "subnet_2" {
-  name                 = "snet-bastion"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
